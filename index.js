611dbcfe809606b0cfd8b00e2d1e0a8685147892
AGNajera <aemiliano9@gmail.com>
2025-10-13 19:04:16 -0600

    feat: implementa script de normalización de texto

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
+// Importamos el nuevo paquete
+const diacritics = require('diacritics');
+const textoEntrada = fs.readFileSync('entrada.txt', 'utf-8');
+// Aplicamos ambas transformaciones en cadena
+const textoNormalizado =
+diacritics.remove(textoEntrada.toLowerCase());
+console.log('Texto normalizado (sin tildes y en minúsculas):');
+console.log(textoNormalizado);
 
-const textoEntrada = fs.readFileSync('entrada.txt', 'utf8');
-console.log('Texto original:', textoEntrada);
-
-const textoProcesado = textoEntrada.toLowerCase();
-console.log('Texto en minúsculas:', textoProcesado);
+// Guardamos el texto normalizado en un nuevo archivo llamado 'salida.txt'
+fs.writeFileSync('salida.txt', textoNormalizado);
+console.log('\n¡ El texto modificado se ha guardado en “salida.txt”');
