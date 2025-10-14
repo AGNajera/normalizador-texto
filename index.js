767512acbf49f099943f125777edae6c49865e2f
AGNajera <aemiliano9@gmail.com>
2025-10-13 18:54:22 -0600

    index sin bun(npm)

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-nul not found
+const fs = require('fs');
+
+const textoEntrada = fs.readFileSync('entrada.txt', 'utf8');
+console.log('Texto original:', textoEntrada);
+
+const textoProcesado = textoEntrada.toLowerCase();
+console.log('Texto en minúsculas:', textoProcesado);
